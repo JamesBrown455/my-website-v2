e75f9ddc02686cba9b11bcc9bbb8dad621a1531a
James Brown <2yankeefans2@gmail.com>
2017-05-31 10:57:53 -0500

    Added Work Page

--- client/main.js
+++ client/main.js
@@ -5,9 +5,10 @@ import './main.html';
 import '/imports/ui/components/header/header.js';
 import '/imports/ui/components/nav/nav.js';
 import '/imports/ui/pages/about/about.js';
-import '/imports/ui/pages/intro/intro.js';
+import '/imports/ui/pages/education/education.js';
 import '/imports/ui/pages/research/research.js';
 import '/imports/ui/pages/contact/contact.js';
+import '/imports/ui/pages/work/work.js';
 
 
 var scrollFunction = function(idstring) {
@@ -17,12 +18,16 @@ var scrollFunction = function(idstring) {
 };
 
 Template.nav.events({
-  "click #introlink": function() {
-    scrollFunction('#intro');
+  "click #educationlink": function() {
+    scrollFunction('#education');
   },
 
   "click #aboutlink": function() {
     scrollFunction('#about');
+  },
+
+  "click #worklink": function() {
+    scrollFunction('#work');
   },
 
   "click #contactlink": function() {
